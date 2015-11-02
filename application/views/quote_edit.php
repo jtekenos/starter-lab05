4053a67797d611e85d75108e8173684ea8daeaa8
jtekenos <jessicatekenos@gmail.com>
2015-11-01 17:27:20 -0800

    finished lab 5 a and b

---
+++ application/views/quote_edit.php
@@ -0,0 +1,13 @@
+<div class="row">	
+	<div class="errors">{message}</div>
+	<form action="/admin/confirm" method="post">
+		<!--
+		<label for="id">ID#</label>
+		<input type="text" id="id" name="id"> </input>
+		-->
+		{fid}
+		{fwho}
+		{fwhat}
+		{fsubmit}
+	</form>
+</div>
